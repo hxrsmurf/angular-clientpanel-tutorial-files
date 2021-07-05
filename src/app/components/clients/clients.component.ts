dd99f880c93b754d51f462d54304283cfbb8c38e
Kevin Murphy <kevin@hxrsmurf.info>
2021-07-04 21:13:31 -0500

    Add clients

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -9,10 +9,20 @@ import { Client } from 'src/app/models/Client';
 })
 export class ClientsComponent implements OnInit {
   clients!: Client[];
+  totalOwed!: number;
 
   constructor(private clientService: ClientService) { }
 
   ngOnInit(): void {
-    this.clientService.getClients().subscribe(clients => this.clients = clients );
+    this.clientService.getClients().subscribe(clients => {
+      this.clients = clients;
+      this.getTotalOwed();
+    } );
+  }
+
+  getTotalOwed(){
+    this.totalOwed = this.clients.reduce((total, client) => {
+      return total + Number(client.balance)
+    }, 0);
   }
 }
